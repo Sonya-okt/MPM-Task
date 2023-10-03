b739a9d9c6fd844374de7d4081cef305d5a82c00
Sonya-okt <143594665+Sonya-okt@users.noreply.github.com>
2023-10-04 00:08:23 +0700

    Update LibAndGui.py

--- LibAndGui.py
+++ LibAndGui.py
@@ -11,8 +11,8 @@ root.configure(background='LightCyan2')
 # StringVar variables
 entry_strvar = StringVar(root)
 # Defining the top
-Label(root, text='PythonGeeks Calculator', font=("Comic Sans MS", 15), bg='LightCyan2').place(x=25, y=0)
-Label(root, text='Press \'x\' twice for exponentiation', font=("Georgia", 10), bg='LightCyan2').place(x=30, y=30)
+Label(root, text='KalkulaTHOR', font=("Comic Sans MS", 15), bg='white').place(x=25, y=0)
+Label(root, text='Press \'x\' twice for exponentiation', font=("Georgia", 10), bg='blue').place(x=30, y=30)
 eqn_entry = Entry(root, justify=RIGHT, textvariable=entry_strvar, width=22, font=12, state='disabled')
 eqn_entry.place(x=10, y=70)
 # Updating root
